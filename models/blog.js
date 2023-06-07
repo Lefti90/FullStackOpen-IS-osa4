90d87f1d9e9891afa597b10d43fa2088b5952293
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-07 20:34:28 +0300

    updated blog.js likes

--- models/blog.js
+++ models/blog.js
@@ -4,7 +4,10 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: 5
+  likes: {
+    type: Number,
+    default: 0
+  },
 })
 
 blogSchema.set('toJSON', {
