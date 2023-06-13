d7b75e7b7eba791282bfe7ee69be7f88f7eb8c64
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-13 09:06:47 +0300

    4_4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,15 @@
-
-
 const dummy = (blogs) => {
-    //return blogs
     return 1
   }
-  
+
+const likes = (blogs) => {
+    const totalLikes = blogs.reduce((sum, blog) => {
+      return sum + blog.likes
+    }, 0)
+    return totalLikes
+  }
+
   module.exports = {
-    dummy
+    dummy,
+    likes
   }
